dc2d155958b0b52b6c46c71a2ab3566a8f1029ec
Caio Figueirôa <cfigueiroa@gmail.com>
2023-01-27 00:59:21 -0300

    feat: add userRoutes and jwt

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import { data } from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 mongoose.set('strictQuery', true);
@@ -25,9 +25,15 @@ const ip = 'localhost';
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, _req, res, _next) => {
+  res.status(500).send({ message: err.message });
+});
 
 await connect();
 
