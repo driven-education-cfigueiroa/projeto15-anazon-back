686e139b28cb1c5bed2113a417974ce670a8a2e6
Caio Figueirôa <cfigueiroa@gmail.com>
2023-01-28 14:53:22 -0300

    feat: add order Route

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
 
+// Generate token for user
 export const generateToken = (user) => {
   return jwt.sign(
     {
@@ -11,4 +12,21 @@ export const generateToken = (user) => {
     process.env.JWT_SECRET,
     { expiresIn: '30d' }
   );
+};
+
+// Check if user is authenticated
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: 'Invalid Token' });
+      } else {
+        req.user = decode;
+        next();
+      }});
+    } else {
+      res.status(401).send({ message: 'No Token' });
+    }
 };
